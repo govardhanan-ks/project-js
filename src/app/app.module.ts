8320c1b8025c3a3c5b9b1b445aa85e2d59f97ad5
LAPTOP-S5478P60\ARUN RAJ.G <arunrajdpm@gmail.com>
2019-02-02 12:46:41 +0530

    Committed for login changes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
+
 import { LoginComponent } from './login/login.component';
 import { VotingComponent } from './voting/voting.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,10 @@ import { VotingComponent } from './voting/voting.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule, MatCheckboxModule,MatCardModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
